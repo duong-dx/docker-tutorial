ccfe63e95c7604f8447fa5336ac70607da84aecc
duongdx <dinh.xuan.duong@sun-asterisk.com>
2022-03-13 20:19:04 +0700

    commit

--- example/docker/mysql/initdb.d/data_test.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO users (name, email, address) VALUES ('Dinh Xuan Duong', 'xuanduong.kma@gmail.com', 'DA-HN');
--- example/docker/mysql/initdb.d/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users(
-	id INT AUTO_INCREMENT,
-	name VARCHAR(255) NULL,
-	email VARCHAR(255) NULL,
-	address VARCHAR(255) NULL,
-	PRIMARY KEY (id)
-);
